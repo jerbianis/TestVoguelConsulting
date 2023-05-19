130ebeea87452b26972f251b28871564cbf1abbe
jerbianis <anisjerbi2@gmail.com>
2023-05-19 11:53:54 +0100

    set route model binding logic for Folders as slug

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Dossier;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -35,6 +36,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+        });
+
+        Route::bind('dossier', function ($value) {
+            return Dossier::where('slug', $value)->firstOrFail();
         });
     }
 
