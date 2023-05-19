e53768fd08ef467a66338b79d5109d7eecfc8be4
jerbianis <anisjerbi2@gmail.com>
2023-05-19 06:12:51 +0100

    Folder and Document migrations and set database

---
+++ database/migrations/2023_05_19_013606_create_documents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('dossier_racine_id')->constrained('dossiers')->onDelete('cascade');
+            $table->string('nom');
+            $table->string('description');
+            $table->string('fichier');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+};
